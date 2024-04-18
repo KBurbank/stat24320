0b3a3b7ffc2a325f1e62c7fb53d3689b5ab57024
Kendra Burbank <burbank@uchicago.edu>
2024-04-17 21:39:20 -0500

    automatically committed render output

--- site_libs/revealjs/plugin/reveal-tablet-chalkboard/plugin.js
+++ site_libs/revealjs/plugin/reveal-tablet-chalkboard/plugin.js
@@ -2141,7 +2141,7 @@ function setPenOrEraserCursor(mode, color) {
 		$('canvas').awesomeCursor('eraser', { color: 'rgba(0,0,0,0.5)' });
 		drawingCanvas[mode].sponge.style.visibility = 'visible';
 	} else {
-		if (drawingCanvas != null) {
+		if (typeof drawingCanvas !== 'undefined') {
 		drawingCanvas[mode].sponge.style.visibility = 'hidden';
 		}
 		$('canvas').awesomeCursor('pencil', { flip: 'vertical', color: pens[mode][color[mode]].color });
